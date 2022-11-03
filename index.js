e7c63de6e6171e2c9f3aa674e2abb11b012d58b7
Timmy Yan <timmyyan2000@gmail.com>
2022-11-02 22:17:58 -0400

    commit tree 9aaa07beacfe038f4a9a836bbbc61d5800a18c42

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch( 'http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message
+    })
+}
